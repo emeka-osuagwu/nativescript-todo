d7def853c7d9ad89fd9fa963bfdd29bc1f76dc9f
Emeka Osuagwu <emekaosuagwu@hotmail.com>
2017-09-10 13:13:12 +0100

    validate user input

--- app/todo/todo.component.ts
+++ app/todo/todo.component.ts
@@ -1,5 +1,5 @@
 import * as dialogs from "ui/dialogs";
-
+import { prompt, PromptResult, inputType } from "ui/dialogs";
 import * as appSettings from "application-settings";
 import { Component, OnInit } from "@angular/core";
 import { ObservableArray } from "tns-core-modules/data/observable-array";
@@ -12,7 +12,7 @@ import { ObservableArray } from "tns-core-modules/data/observable-array";
 
 export class TodoComponent implements OnInit {
 
-	private todos: any = [{"hjcdfvdf": "fdfvdf"}, ]
+	todos: any;
 
     constructor
     (
@@ -22,19 +22,46 @@ export class TodoComponent implements OnInit {
 
     ngOnInit() {
 
+    	if (appSettings.getString('todos') == '' || appSettings.getString('todos') == null) 
+    	{
+    		console.log('fvkdfjh')	
+    	}
+    	else
+    	{
+    		this.todos = JSON.parse(appSettings.getString('todos'));
+    		console.log(JSON.stringify(this.todos))
+    	}
+
     }
+
 
     addTodo(){
     	let options = {
-    	    title: "Name",
-    	    defaultText: "Enter your name",
+    	    title: "Enter Item",
+    	    defaultText: "",
     	    inputType: inputType.text,
     	    okButtonText: "Ok",
     	    cancelButtonText: "Cancel"
     	};
 
     	prompt(options).then((result: PromptResult) => {
-    	    console.log("Hello, " + result.text);
+
+    		if (result.text == null || result.text == '') 
+    		{
+    			alert('you need to enter an item to create a todo :)')	
+    		}
+    		else
+    		{    			   
+				var new_item = {
+					id: Math.floor(Math.random() * 10000) + 1,
+					value: result.text,
+					status: 'pending'
+				}
+
+				appSettings.setString('todos', JSON.stringify(new_item))
+
+			    console.log(JSON.stringify(new_item));
+    		}
     	});
     }
 
